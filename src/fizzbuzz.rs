2467fc41c47e7f9fc022c6a681215bb5d9edf5ee
Nicholas Beach <nicholascbeach@gmail.com>
2017-01-01 19:17:45 -0500

    Moved fizzbuzz to appropriately named file and gave test suite a meaningful name

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -14,29 +14,29 @@ pub fn fizzbuzz(number: i32) -> String {
 }
 
 #[cfg(test)]
-mod tests {
+mod fizzbuzz_test {
     use super::fizzbuzz;
 
     #[test]
-    fn fizzbuzz_when_a_number_is_given_returns_the_number() {
+    fn when_a_number_is_given_returns_the_number() {
         assert_eq!(fizzbuzz(1), "1");
         assert_eq!(fizzbuzz(4), "4");
     }
 
     #[test]
-    fn fizzbuzz_when_a_multiple_of_3_is_given_returns_fizz() {
+    fn when_a_multiple_of_3_is_given_returns_fizz() {
         assert_eq!(fizzbuzz(3), "fizz");
         assert_eq!(fizzbuzz(9), "fizz");
     }
 
     #[test]
-    fn fizzbuzz_when_a_multiple_of_5_is_given_returns_fizz() {
+    fn when_a_multiple_of_5_is_given_returns_fizz() {
         assert_eq!(fizzbuzz(10), "buzz");
         assert_eq!(fizzbuzz(25), "buzz");
     }
 
     #[test]
-    fn fizzbuzz_when_a_multiple_of_3_and_5_is_given_returns_fizzbuzz() {
+    fn when_a_multiple_of_3_and_5_is_given_returns_fizzbuzz() {
         assert_eq!(fizzbuzz(15), "fizzbuzz");
         assert_eq!(fizzbuzz(30), "fizzbuzz");
     }
